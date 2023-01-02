1ee7b815c72bce6ab3083c74ec049807fadb1ed7
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2023-01-02 18:03:13 +0200

    Rustify stuff

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -7,70 +7,71 @@ struct Elf {
     calories: Vec<Calories>
 }
 
-fn toElf(elfStuff: &str) -> Elf {
-    let calories: Vec<i32> = elfStuff.split("\n").map(|c| c.parse::<i32>().unwrap()).collect();
+fn to_elf(elf_stuff: &str) -> Elf {
+    let calories: Vec<i32> = elf_stuff.split("\n").map(|c| c.parse::<i32>().unwrap()).collect();
     return Elf {
-        calories: calories
+        calories
     }
 }
 
-fn parseElves(input: String) -> Vec<Elf> {
-    return input.split("\n\n").filter(|s| s.len() > 0).map(|s| toElf(s)).collect();
+fn parse_elves(input: String) -> Vec<Elf> {
+    return input.split("\n\n").filter(|s| s.len() > 0).map(|s| to_elf(s)).collect();
 }
 
-fn readElvesInput() -> String {
+fn read_elves_input() -> String {
     let args: Vec<String> = env::args().collect();
-    let mut filePath = env::current_dir().unwrap();
+    let mut file_path = env::current_dir().unwrap();
 
-    let defaultFile = "day1_input.txt".to_owned();
-    filePath.push(args.get(1).unwrap_or(&defaultFile));
+    let default_file = "day1_input.txt".to_string();
+    file_path.push(args.get(1).unwrap_or(&default_file));
 
-    return fs::read_to_string(filePath).expect("Should have been able to read the file. Is the file path correct?");
+    return fs::read_to_string(file_path).expect("Should have been able to read the file. Is the file path correct?");
 }
 
 fn first() {
-    let contents = readElvesInput();
-    let elves = parseElves(contents);
+    let contents = read_elves_input();
+    let elves = parse_elves(contents);
 
     println!("Elves count: {}", elves.len());
 
-    let mut maxCalories: i32 = 0;
+    let mut max_calories: i32 = 0;
     for elf in elves {
-        let currentElfSum = elf.calories.iter().sum();
+        let current_elf_sum = elf.calories.iter().sum();
 
-        if currentElfSum > maxCalories {
-            maxCalories = currentElfSum
+        if current_elf_sum > max_calories {
+            max_calories = current_elf_sum
         }
     }
 
-    println!("Max calories for elf = {}", maxCalories)
+    println!("Max calories for elf = {max_calories}")
 }
 
 fn second() {
-    let contents = readElvesInput();
-    let elves = parseElves(contents);
+    let contents = read_elves_input();
+    let elves = parse_elves(contents);
 
     println!("Elves count: {}", elves.len());
 
 
-    let mut currentMinIdx = 0;
-    let mut maxCalories: [i32; 3] = [0, 0, 0];
+    let mut current_min_idx = 0;
+    let mut max_calories: [i32; 3] = [0, 0, 0];
     for elf in elves {
-        let currentElfSum = elf.calories.iter().sum();
+        let current_elf_sum = elf.calories.iter().sum();
 
-        let minElf = maxCalories[currentMinIdx];
-        if currentElfSum > minElf {
-            maxCalories[currentMinIdx] = currentElfSum;
+        let min_elf = max_calories[current_min_idx];
+        if current_elf_sum > min_elf {
+            max_calories[current_min_idx] = current_elf_sum;
 
             for i in 0..3 {
-                if maxCalories[i] < maxCalories[currentMinIdx] {
-                    currentMinIdx = i;
+                if max_calories[i] < max_calories[current_min_idx] {
+                    current_min_idx = i;
                 }
             }
         }
     }
 
-    println!("Max calories for elf = {:?}. Sum = {}", maxCalories, maxCalories.iter().sum::<i32>())
+    let sum_calories = max_calories.iter().sum::<i32>();
+    println!("Max calories for elf = {max_calories:?}. Sum = {sum_calories}");
 }
 
 fn main() {
